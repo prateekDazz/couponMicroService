f92666d9c07118f443a8cfa4e80f38cf25d32958
prateekDazz <146050363+prateekDazz@users.noreply.github.com>
2024-09-01 20:39:43 +0530

    add sample test case

--- src/main/java/com/coupon/coupon/CouponApplication.java
+++ src/main/java/com/coupon/coupon/CouponApplication.java
@@ -1,13 +1,24 @@
 package com.coupon.coupon;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class CouponApplication {
+	Logger logger = LoggerFactory.getLogger(CouponApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(CouponApplication.class, args);
+	}
+	@PostConstruct
+	
+	public void init()
+	{
+		logger.info("started the application and create the bean");
 	}
 
 }
--- src/test/java/com/coupon/coupon/CouponApplicationTests.java
+++ src/test/java/com/coupon/coupon/CouponApplicationTests.java
@@ -1,13 +1,28 @@
 package com.coupon.coupon;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CouponApplicationTests {
+	Logger logger = LoggerFactory.getLogger(CouponApplicationTests.class);
+
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	@DisplayName("sample Test Method")
+	void sample()
+	{
+		logger.info("executing Test");
+		assertEquals(true, true);
 	}
 
 }
